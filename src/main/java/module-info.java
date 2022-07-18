f047e8089090fd2a2ec19940413f40bdf5e76ce4
NTKong <kong1032002@gmail.com>
2022-07-18 14:32:36 +0700

    reset repo

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.quiz {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires mysql.connector.java;
-
-    opens com.quiz to javafx.fxml;
-    exports com.quiz;
-}
